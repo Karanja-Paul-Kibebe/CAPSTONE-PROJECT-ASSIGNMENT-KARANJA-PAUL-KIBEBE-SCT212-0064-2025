1bac0ec71e13c64da18ec0f972df3b0ff7529a2c
Karanja-Paul-Kibebe <karanjapaul461@gmail.com>
2026-02-08 09:53:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ function rejectCookies() {
 const signUpForm = document.getElementById('signupForm');
 
 signUpForm.addEventListener('submit', (e) => {
-  e.preventDefault();
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
